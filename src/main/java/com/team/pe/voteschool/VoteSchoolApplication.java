83a49b00c683535571941558cb4e374b88b06c91
Krhone-bit <alvaroupeu@gmail.com>
2023-09-28 18:24:16 -0500

    first commit 1.0

---
+++ src/main/java/com/team/pe/voteschool/VoteSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.team.pe.voteschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/pe/voteschool/VoteSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.pe.voteschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
